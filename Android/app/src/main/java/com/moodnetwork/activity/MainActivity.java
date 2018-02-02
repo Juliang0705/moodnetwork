0058e147e739c88b2de81d42cef96f86a669584b
Tyler J. Durden <durden@tamu.edu>
2018-02-02 13:10:11 -0600

    now properly requests permissions for API level >= 23

--- Android/app/src/main/java/com/moodnetwork/activity/MainActivity.java
+++ Android/app/src/main/java/com/moodnetwork/activity/MainActivity.java
@@ -1,8 +1,12 @@
 package com.moodnetwork.activity;
 
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.Manifest;
 
 import com.moodnetwork.service.*;
 
@@ -10,12 +14,66 @@ import com.moodnetwork.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Not sure what this return code is actually supposed to be besides a final int that's >= 0
+    private final int MY_PERMISSIONS_REQUEST_FINE_LOCATION = 0;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // location-related task you need to do.
+
+                    startBackgroundServices();
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+
+                    // tell the user they need to accept the permissions to use the app?
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        startBackgroundServices();
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_FINE_LOCATION);
+
+                // MY_PERMISSIONS_REQUEST_FINE_LOCATION is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+
+        // startBackgroundServices();
     }
 
     private void startBackgroundServices() {
